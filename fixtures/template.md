afec391b486bc4ede89dd9a569d1aa88a3aa0f70
brianrossetti <brossetti1@gmail.com>
2022-11-30 14:59:53 -0500

    adding comment parsing and setting up probot app in data-preservation-programs

---
+++ fixtures/template.md
@@ -0,0 +1,31 @@
+## Stats & Info for DataCap Allocation
+
+#### Multisig Notary address
+> f01858410
+
+#### Client address
+> f1bstbq5bi72kyovhh7zoo2f6l6uivsjz4ey5dnqq
+
+
+#### Last two approvers
+> **kernelogic** & **cryptowhizzard**
+
+
+#### Rule to calculate the allocation request amount
+> 400% of weekly dc amount requested
+
+#### DataCap allocation requested
+> 400TiB
+
+#### Total DataCap granted for client so far
+> 350TiB
+
+#### Datacap to be granted to reach the total amount requested by the client (5 PiB)
+> 4.65PiB
+
+#### **[Stats](https://filplus.d.interplanetary.one/clients?filter=t01943349 "Go to stats")**
+| Number of deals  | Number of storage providers | Previous DC Allocated  |  Top provider | Remaining DC
+|---|---|---|---|---|
+| 6575  | 26  |  200TiB | 18.26  | 42.28TiB
+
+_Originally posted by @large-datacap-requests[bot] in https://github.com/filecoin-project/filecoin-plus-large-datasets/issues/917#issuecomment-1315037140_
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,34 @@
 import { ApplicationFunction, Probot } from 'probot'
 import { ApplicationFunctionOptions } from 'probot/lib/types'
+import { parseIdentifiers } from './identifiers'
 
 const handler: ApplicationFunction = (app: Probot, options: ApplicationFunctionOptions): void => {
   app.log('Yay, the app was loaded!')
   app.log('options', options)
+
+  app.on(["issue_comment.created"], async (context) => {
+    app.log.info({context});
+    const { body } = context.payload.comment;
+
+    if (body.match(/Stats & Info for DataCap Allocation/i)) {
+      const { notaryAddress, clientAddress, interplanetaryLink, datasetIssueLink } = parseIdentifiers(app, body)
+
+      app.log.info({notaryAddress, clientAddress, interplanetaryLink, datasetIssueLink})
+
+      const issueComment = context.issue({
+        body: JSON.stringify({
+          comment: "Thanks for opening this issue!",
+          notaryAddress,
+          clientAddress,
+          interplanetaryLink,
+          datasetIssueLink
+        })
+      });
+
+      app.log.info({issueComment})
+      await context.octokit.issues.createComment(issueComment);
+    }
+  });
 }
 
 export = handler
