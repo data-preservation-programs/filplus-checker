1639ad0cf7adc3c335ed23343b357f77929638be
Xinan Xu <39632914+xinaxu@users.noreply.github.com>
2022-12-06 17:03:04 -0800

    remove fit

--- tests/charts/GeoMap.spec.ts
+++ tests/charts/GeoMap.spec.ts
@@ -2,7 +2,7 @@ import * as fs from "fs";
 import GeoMap, {GeoMapEntry} from "../../src/charts/GeoMap";
 
 describe('GeoMap', () => {
-  fit('should generate a chart image', async () => {
+  it('should generate a chart image', async () => {
     const data: GeoMapEntry[] = [
       {
         label: 'f01000',
