7a6096b0c8666c90af0c216abc60a2bca6b3b0fe
Xinan Xu <39632914+xinaxu@users.noreply.github.com>
2022-12-14 09:13:17 -0800

    Unit test to manually trigger the report

---
+++ tests/ManualTrigger.spec.ts
@@ -0,0 +1,17 @@
+import {manualTrigger} from "../src/ManualTrigger";
+
+xdescribe('manualTrigger', () => {
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 86400000
+  it ('should trigger the check', async () => {
+    const i = '1'
+    console.log(`Triggering ${i}`)
+    const event = {
+      queryStringParameters: {
+        issueId: i.toString()
+      }
+    }
+    const body = await manualTrigger(event as any, {} as any)
+    console.log(body.statusCode)
+    console.log(`Finished ${i}`)
+  })
+})
