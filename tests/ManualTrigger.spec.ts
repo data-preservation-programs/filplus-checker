7c76e28c39079bba86b15dfca2e851874d18b355
Xinan Xu <39632914+xinaxu@users.noreply.github.com>
2022-12-14 22:59:57 -0800

    fix

--- tests/ManualTrigger.spec.ts
+++ tests/ManualTrigger.spec.ts
@@ -3,15 +3,16 @@ import {manualTrigger} from "../src/ManualTrigger";
 xdescribe('manualTrigger', () => {
   jasmine.DEFAULT_TIMEOUT_INTERVAL = 86400000
   it ('should trigger the check', async () => {
-    const i = '1'
-    console.log(`Triggering ${i}`)
-    const event = {
-      queryStringParameters: {
-        issueId: i.toString()
+    for (let i = 0; i <= 0; i++) {
+      console.log(`Triggering ${i}`)
+      const event = {
+        queryStringParameters: {
+          issueId: i.toString()
+        }
       }
+      const body = await manualTrigger(event as any, {} as any)
+      console.log(body.statusCode)
+      console.log(`Finished ${i}`)
     }
-    const body = await manualTrigger(event as any, {} as any)
-    console.log(body.statusCode)
-    console.log(`Finished ${i}`)
   })
 })
